98edfc971082e3bc31a2a18399f67ecb9257a75a
BrianOdhiambo <brianodhiambo530@gmail.com>
2023-11-23 22:22:35 +0300

    Added pages folder

---
+++ src/pages/courses/CoursesPage.tsx
@@ -0,0 +1,5 @@
+const CoursesPage = () => {
+    return <section className="section h-[500px]">CoursesPage</section>;
+  };
+  
+  export default CoursesPage;
---
+++ src/pages/pricing/PricingPage.tsx
@@ -0,0 +1,5 @@
+const PricingPage = () => {
+    return <section className="section h-[500px]">PricingPage</section>;
+  };
+  
+  export default PricingPage;
