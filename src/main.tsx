1a2ceaebc1e5faa5d4e49fb499d8f1318cb52fd3
Felix orinda <forinda82@gmail.com>
2023-12-14 22:57:02 +0300

    Add service modules

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { Provider as ReduxProvider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import store, { persistor } from './store/index';
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<ReduxProvider store={store}>
+			<PersistGate persistor={persistor}>
+				<QueryClientProvider client={queryClient}>
+					<App />
+				</QueryClientProvider>
+			</PersistGate>
+		</ReduxProvider>
+	</React.StrictMode>
+);
