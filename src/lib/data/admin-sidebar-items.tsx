2db292b30d8c47545eddf41fca3b2b4c7d6dc8b5
Felix Orinda <forinda82@gmail.com>
2024-02-21 05:23:49 +0300

    Refactor urls

--- src/lib/data/admin-sidebar-items.tsx
+++ src/lib/data/admin-sidebar-items.tsx
@@ -1,46 +1,142 @@
 import { MenuItemProps } from '@/components/shared/DashboardMenuItem';
-import { LucideFileCog, LucideGraduationCap, LucideLayoutDashboard, LucideLibrary, LucideMessageCircle, LucidePhone, LucideStar, LucideUser } from 'lucide-react';
+import { LucideAsteriskSquare, LucideFileCog, LucideGlobe, LucideGraduationCap, LucideLayoutDashboard, LucideLibrary, LucideMessageCircle, LucidePhone, LucidePlusSquare, LucideShare, LucideStar, LucideUser } from 'lucide-react';
 
 export const adminSidebarItems: MenuItemProps[] = [
 	{
 		Icon: LucideLayoutDashboard,
 		name: 'Dashboard',
-		href: '/student/dashboard'
+		href: '/org-admin/dashboard'
 	},
 	{
 		Icon: LucideLibrary,
 		name: 'Courses',
-		href: '/student/dashboard/courses'
+		href: '/org-admin/dashboard/courses',
+		sub_items: [
+			{
+				name: 'All Courses',
+				href: '/org-admin/dashboard/courses',
+				Icon: LucideAsteriskSquare
+			},
+			{
+				name: 'Add Course',
+				href: '/org-admin/dashboard/courses/add',
+				Icon: LucidePlusSquare
+			}
+		]
+	},
+	{
+		name: 'Instructors',
+		Icon: LucideGlobe,
+		href: '/org-admin/dashboard/instructors',
+		sub_items: [
+			{
+				name: 'All Instructors',
+				href: '/org-admin/dashboard/students',
+				Icon: LucideAsteriskSquare
+			},
+			{
+				name: 'Add Instructor',
+				href: '/org-admin/dashboard/students/add',
+				Icon: LucidePlusSquare
+			},
+			{
+				name: 'Invite Instructor',
+				href: '/org-admin/dashboard/students/groups',
+				Icon: LucideShare
+			}
+		]
+	},
+	{
+		Icon: LucideGraduationCap,
+		name: 'Students',
+		href: '/org-admin/dashboard/students',
+		sub_items: [
+			{
+				name: 'All Students',
+				href: '/org-admin/dashboard/students',
+				Icon: LucideAsteriskSquare
+			},
+			{
+				name: 'Add Student',
+				href: '/org-admin/dashboard/students/add',
+				Icon: LucidePlusSquare
+			},
+			{
+				name: 'Invite Student',
+				href: '/org-admin/dashboard/students/groups',
+				Icon: LucideShare
+			}
+		]
+	},
+	{
+		name:"Categories",
+		Icon: LucideGlobe,
+		href: "/org-admin/dashboard/categories",
+		sub_items: [
+			{
+				name: "All Categories",
+				href: "/org-admin/dashboard/categories",
+				Icon: LucideAsteriskSquare
+			},
+			{
+				name: "Add Category",
+				href: "/org-admin/dashboard/categories/add",
+				Icon: LucidePlusSquare
+			}
+		]
+	},
+	// users
+	{
+		Icon: LucideUser,
+		name: 'Users',
+		href: '/org-admin/dashboard/users',
+		sub_items: [
+			{
+				name: 'All Users',
+				href: '/org-admin/dashboard/users',
+				Icon: LucideAsteriskSquare
+			},
+			{
+				name: 'Add User',
+				href: '/org-admin/dashboard/users/add',
+				Icon: LucidePlusSquare
+			},
+			{
+				name: 'Invite User',
+				href: '/org-admin/dashboard/users/groups',
+				Icon: LucideShare
+			}
+		]
 	},
 	{
 		Icon: LucideMessageCircle,
-		href: '/student/dashboard/notifications',
+		href: '/org-admin/dashboard/notifications',
 		name: 'Notifications'
 	},
 	{
 		Icon: LucideGraduationCap,
-		href: '/student/dashboard/certificates',
+		href: '/org-admin/dashboard/certificates',
 		name: 'Certificates'
 	},
 	{
 		Icon: LucideStar,
-		href: '/student/dashboard/reviews',
+		href: '/org-admin/dashboard/reviews',
 		name: 'Reviews'
 	},
 	{
 		Icon: LucideUser,
 		name: 'Profile',
-		href: '/student/dashboard/profile'
+		href: '/org-admin/dashboard/profile'
 	},
 
 	{
 		Icon: LucideFileCog,
-		href: '/student/dashboard/settings',
+		href: '/org-admin/dashboard/settings',
 		name: 'Settings'
 	},
 	{
 		Icon: LucidePhone,
 		name: 'Help',
-		href: '/student/dashboard/help'
+		href: '/org-admin/dashboard/help'
 	}
 ];
