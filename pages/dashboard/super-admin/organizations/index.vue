4ae69f2435722fb3f483f73323d6c12dcce2d537
Felix Orinda <forinda82@gmail.com>
2024-04-02 23:49:34 +0300

    Add data loader indicator

--- pages/dashboard/super-admin/organizations/index.vue
+++ pages/dashboard/super-admin/organizations/index.vue
@@ -9,15 +9,19 @@ definePageMeta({
 	layout: "super-admin-layout"
 });
 type OrganizationType = {
-  name: string
-  logo?: string
+	name: string
+	logo?: string
 }
+const isLoading = ref(false);
 const fetchOrganizations = async () => {
+	isLoading.value = true;
 	try {
 		const feedback = await api.get("/organizations/");
 		organizations.value = feedback.data;
 	} catch (error) {
 		console.log(error);
+	} finally {
+		isLoading.value = false;
 	}
 };
 
@@ -29,25 +33,28 @@ onMounted(() => {
 
 });
 
+const haveOrganizations = computed(() => organizations.value.length > 0);
 </script>
 <template>
-  <div>
-
-    <div class="flex justify-between items-center border p-4 my-2 rounded">
-      <h1 class="text-2xl font-bold">Organizations</h1>
-      <nuxt-link :to="{ name: 'dashboard-super-admin-organizations-new' }" class="px-4 py-2 rounded bg-primary">Add
-        new</nuxt-link>
-    </div>
-    <div class="grid gap-4 grid-cols-[repeat(auto-fit,minmax(20rem,1fr))]">
-      <div v-for="organization of organizations" :key="organization!.name"
-        class="bg-white overflow-hidden shadow-sm rounded border">
-        <img :src="organization.logo ?? 'https://i.pinimg.com/236x/87/d7/e2/87d7e20741adb00322ab7b09122d8b79.jpg'"
-          alt="Organization logo" class="w-full h-40 object-cover rounded-tl rounded-tr">
-        <div class="p-4">
-          <h2 class="text-lg font-bold mt-2">{{ organization.name }}</h2>
-        </div>
-      </div>
-    </div>
-
-  </div>
+	<div>
+		<div v-if="!isLoading && haveOrganizations" class="grid gap-4 grid-cols-[repeat(auto-fit,minmax(20rem,1fr))]">
+			<div v-for="organization of organizations" :key="organization!.name"
+				class="bg-white overflow-hidden shadow-sm rounded border">
+				<img :src="organization.logo ?? 'https://i.pinimg.com/236x/87/d7/e2/87d7e20741adb00322ab7b09122d8b79.jpg'"
+					alt="Organization logo" class="w-full h-40 object-cover rounded-tl rounded-tr">
+				<div class="p-4">
+					<h2 class="text-lg font-bold mt-2">{{ organization.name }}</h2>
+				</div>
+			</div>
+		</div>
+		<div v-else-if="isLoading" class="flex justify-center items-center">
+			<partial-loader />
+		</div>
+		<div v-else class="flex justify-center items-center flex-col gap-8 p-10 min-h-[60vh] ">
+			<h1 class="text-8xl font-extrabold tracking-widest opacity-10">
+				404
+			</h1>
+			<p class="text-3xl opacity-60">No organizations found</p>
+		</div>
+	</div>
 </template>
