426f60dc4fb8dc015cf4c0b53b17177284a08ccc
Felix Orinda <forinda82@gmail.com>
2024-04-02 23:48:27 +0300

    Remove eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,55 +1,54 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-				experimental: {
-								typedPages: true
-				},
-				devtools: {
-				 enabled: true,
+	experimental: {
+		typedPages: true
+	},
+	devtools: {
+		enabled: true,
 
-				 timeline: {
-					 enabled: true
-					}
-				},
-				ssr: false,
-				extends: [],
-				modules: [
-								"@nuxtjs/i18n",
-								"@vueuse/nuxt",
-								"@nuxt/image",
-								"@nuxtjs/google-fonts",
-								"@pinia/nuxt",
-								"@pinia-plugin-persistedstate/nuxt",
-								"nuxt-icon",
-								"nuxt-headlessui",
-								"nuxt-lucide-icons",
-								"my-module",
-								"@nuxtjs/eslint-module"
-				],
-				css: ["~/assets/css/main.css", "sweetalert2/src/sweetalert2.scss"],
-				postcss: {
-								plugins: {
-												tailwindcss: {},
-												autoprefixer: {}
-								}
-				},
-				routeRules: {},
-				googleFonts: {
-								families: {
-												Inter: true,
-												Catamaran: true,
-												Poppins: true,
-												Raleway: true,
-												Roboto: true
-								}
-				},
-				lucide: {
-								namePrefix: "icon"
-				},
-				headlessui: {
-								prefix: "hui"
-				},
-				pinia: {
-								disableVuex: true,
-								storesDirs: ["./stores/**"]
-				}
+		timeline: {
+			enabled: true
+		}
+	},
+	ssr: false,
+	extends: [],
+	modules: [
+		"@nuxtjs/i18n",
+		"@vueuse/nuxt",
+		"@nuxt/image",
+		"@nuxtjs/google-fonts",
+		"@pinia/nuxt",
+		"@pinia-plugin-persistedstate/nuxt",
+		"nuxt-icon",
+		"nuxt-headlessui",
+		"nuxt-lucide-icons",
+		"my-module"
+	],
+	css: ["~/assets/css/main.css", "sweetalert2/src/sweetalert2.scss"],
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {}
+		}
+	},
+	routeRules: {},
+	googleFonts: {
+		families: {
+			Inter: true,
+			Catamaran: true,
+			Poppins: true,
+			Raleway: true,
+			Roboto: true
+		}
+	},
+	lucide: {
+		namePrefix: "icon"
+	},
+	headlessui: {
+		prefix: "hui"
+	},
+	pinia: {
+		disableVuex: true,
+		storesDirs: ["./stores/**"]
+	}
 });
