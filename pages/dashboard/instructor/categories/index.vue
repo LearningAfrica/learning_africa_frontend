dc2178d5a1ed17b04b847e9e1c48873f33541084
Felix Orinda <forinda82@gmail.com>
2024-04-16 23:54:21 +0300

    Update an delete category

--- pages/dashboard/instructor/categories/index.vue
+++ pages/dashboard/instructor/categories/index.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import moment from "moment";
 import type { PaginationData } from "~/types/response";
+
+
+definePageMeta({
+	layout: "instructor-layout",
+});
 const isLoading = ref(false);
 const { $privateAxios} = useNuxtApp();
 
@@ -19,11 +24,60 @@ onMounted(async () => {
 	}
 });
 
+const selectedCategory = ref<Pick<PaginationData,"categories">["categories"]["data"][number]>();
 
+const isUpdateModalOpen = ref(false);
+const isDeleteModalOpen = ref(false);
+const isUpdating = ref(false);
+const isDeleting = ref(false);
+const selectCategoryToUpdate = (id:number)=>{
+	selectedCategory.value = categories.value!.data.find((cat)=>cat.id === id);
+	toggleDeleteModal(false)
+	toggleUpdateModal(true);
+};
 
-definePageMeta({
-	layout: "instructor-layout",
-});
+const selectCategoryToDelete = (id:number)=>{
+	selectedCategory.value = categories.value!.data.find((cat)=>cat.id === id);
+	toggleUpdateModal(false)
+	toggleDeleteModal(true);
+};
+
+const toggleUpdateModal=(state:boolean)=>{
+	isUpdateModalOpen.value = state;
+
+};
+
+const toggleDeleteModal=(state:boolean)=>{
+	isDeleteModalOpen.value = state;
+};
+
+const handleDeleteCategory = async ()=>{
+	isDeleting.value = true;
+	try {
+		await $privateAxios.delete(`/api/categories/${selectedCategory.value!.id}/`);
+		await $notify.fire("Category deleted", "success");
+	} catch (error) {
+		console.log(error);
+		$notify.fire("Failed to delete category", "error");
+	} finally {
+		isDeleting.value = false;
+		toggleDeleteModal(false);
+	}
+};
+
+const handleUpdateCategory = async ()=>{
+	isUpdating.value = true;
+	try {
+		await $privateAxios.put(`/api/categories/${selectedCategory.value!.id}/`,selectedCategory.value);
+		await $notify.fire("Category updated", "success");
+	} catch (error) {
+		console.log(error);
+		await $notify.fire("Failed to update category", "error");
+	} finally {
+		isUpdating.value = false;
+		toggleUpdateModal(false);
+	}
+};
 </script>
 <template>
 	<div class="py-4 flex flex-col gap-4">
@@ -70,9 +124,13 @@ definePageMeta({
 						</td>
 						<td>
 							<div class="flex gap-4 justify-center">
-								<nuxt-link :to="'/dashboard/student/courses/' + cat.id"
-									class="bg-primary py-1 px-4 rounded-sm">View</nuxt-link>
-								<button class="border-primary border py-1 px-4 rounded-sm">Edit</button>
+							
+								<button class="border-primary border py-1 px-4 rounded-sm"
+								@click="selectCategoryToUpdate(cat.id)"
+								>Update</button>
+								<button class="border-red-500 border py-1 px-4 rounded-sm"
+								@click="selectCategoryToDelete(cat.id)"
+								>Delete</button>
 							</div>
 						</td>
 					</tr>
@@ -80,6 +138,71 @@ definePageMeta({
 				</tbody>
 			</table>
 		</div>
+		<hui-dialog :open="isDeleteModalOpen" @close="toggleDeleteModal(false)"
+		class="fixed inset-0 z-[999] overflow-y-auto">
+		<div class="flex items-center justify-center min-h-screen w-screen bg-white bg-opacity-70 border ">
+			<div class="bg-white border p-6 rounded-lg shadow-lg  w-full min-h-[20vh] max-w-xl flex gap-4 flex-col">
+				<hui-dialog-panel>
+					<hui-dialog-title class="text-lg font-bold my-2 text-center border bg-red-500 text-white rounded">
+						Delete {{ selectedCategory!.title }}
+					</hui-dialog-title>
+					<hui-dialog-description class="text-gray-600 ">
 
+						Are you sure you want to delete this organization?
+						This action cannot be undone. All data associated with this organization will be lost.
+					</hui-dialog-description>
+					<form action="" class="w-full"
+						@submit.prevent.stop="handleDeleteCategory">
+						<!-- <div class="w-full">
+							<input type="text" v-model="selectedOrganization!.name"
+								class="w-full rounded border-blue-200 border-2 ">
+						</div> -->
+
+						<div class="flex justify-center mt-6 gap-4">
+							<button v-if="!isDeleting" type="button" @click="toggleDeleteModal(false)"
+								class="px-4 py-2 text-sm bg-gray-400 hover:bg-gray-600 border rounded-md">Cancel</button>
+							<button type="submit" :disabled="isDeleting" :class="{ 'cursor-not-allowed': isDeleting }"
+								class="px-4 py-2 
+							text-sm bg-red-500 hover:bg-red-700 transition-all ease-in duration-150 rounded-md text-white">
+								{{ isDeleting ? "Deleting" : "Delete" }}
+							</button>
+						</div>
+					</form>
+				</hui-dialog-panel>
+			</div>
+		</div>
+	</hui-dialog>
+	<hui-dialog :open="isUpdateModalOpen" @close="toggleUpdateModal(false)"
+		class="fixed inset-0 z-[999] overflow-y-auto">
+		<div class="flex items-center justify-center min-h-screen w-screen bg-white bg-opacity-70 border ">
+			<div class="bg-white border p-6 rounded-lg shadow-lg  w-full min-h-[20vh] max-w-xl">
+				<hui-dialog-panel>
+					<hui-dialog-title class="text-lg font-bold my-2">
+						{{ selectedCategory!.title }}
+					</hui-dialog-title>
+					<hui-dialog-description class="text-gray-600">
+
+
+					</hui-dialog-description>
+					<form action="" class="w-full" @submit.prevent.stop="handleUpdateCategory">
+						<div class="w-full">
+							<input type="text" v-model="selectedCategory!.title"
+								class="w-full rounded border-blue-200 border-2 ">
+						</div>
+
+						<div class="flex justify-start mt-6 gap-4">
+							<button v-if="!isUpdating" type="button" @click="toggleUpdateModal(false)"
+								class="px-4 py-2 text-sm bg-gray-400 border rounded-md">Cancel</button>
+							<button type="submit" :disabled="isUpdating" :class="{ 'cursor-not-allowed': isUpdating }"
+								class="px-4 py-2 
+							text-sm bg-primary rounded-md text-white">
+								{{ isUpdating ? "Updating" : "Save changes" }}
+							</button>
+						</div>
+					</form>
+				</hui-dialog-panel>
+			</div>
+		</div>
+	</hui-dialog>
 	</div>
 </template>
